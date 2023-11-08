1c96f53808b160399e70c310b93cba6e8a442e2a
binscup <bintang684@smk.belajar.id>
2023-11-08 10:04:53 +0700

    first commit

---
+++ resources/views/peminjaman-device/returndevice.blade.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>bazma</title>
+    <link rel="stylesheet" href="./style/style.css">
+    <link rel="stylesheet" href="./js/java.js">
+    <link href="./dist/css/tabler.min.css?1684106062" rel="stylesheet" />
+    <link href="./dist/css/tabler-flags.min.css?1684106062" rel="stylesheet" />
+    <link href="./dist/css/tabler-payments.min.css?1684106062" rel="stylesheet" />
+    <link href="./dist/css/tabler-vendors.min.css?1684106062" rel="stylesheet" />
+    <link href="./dist/css/demo.min.css?1684106062" rel="stylesheet" />
+    <link rel="stylesheet" href="/style/responsive.css">
+    <script src="./dist/js/demo-theme.min.js?1684106062"></script>
+
+</head>
+
+<body>
+    <div class="balik">
+        <a href="elekronik.html" class="next round" style="text-decoration: none;">
+            < </a>
+    </div>
+    <div class="title">
+        <h1><b>Return Device</b</h1>
+                <h2><b>Berikut ini :</b></h2>
+    </div>
+    <div class="grid-container">
+        <div class="cardGrid">
+            <div class="interGrid">
+                <img src="/asset/picture/Laptop.png" alt="">
+                <p>Laptop</p>
+            </div>
+            <div class="cardButton">
+                <a href="" style="text-decoration: none;">pilih</a>
+            </div>
+        </div>
+        <div class="cardGrid">
+            <div class="interGrid">
+                <img src="/asset/picture/Headphones.png" alt="">
+                <p>Earphone</p>
+            </div>
+            <div class="cardButton">
+                <a href="" style="text-decoration: none;">pilih</a>
+            </div>
+        </div>
+        <div class="cardGrid">
+            <div class="interGrid">
+                <img src="/asset/picture/Tablet.png" alt="">
+                <p>Tablet</p>
+            </div>
+            <div class="cardButton">
+                <a href="" style="text-decoration: none;">pilih</a>
+            </div>
+        </div>
+        <div class="cardGrid">
+            <div class="interGrid">
+                <img src="/asset/picture/Flashdisk.png" alt="">
+                <p>Flashdisk</p>
+            </div>
+            <div class="cardButton">
+                <a href="" style="text-decoration: none;">pilih</a>
+            </div>
+        </div>
+        <div class="cardGrid">
+            <div class="interGrid">
+                <img src="/asset/picture/Printer.png" alt="">
+                <p>Printer</p>
+            </div>
+            <div class="cardButton">
+                <a href="" style="text-decoration: none;">pilih</a>
+            </div>
+        </div>
+        <div class="cardGrid">
+            <div class="interGrid">
+                <img src="/asset/picture/Camera.png" alt="">
+                <p>Camera   </p>
+            </div>
+            <div class="cardButton">
+                <a href="" style="text-decoration: none;">pilih</a>
+            </div>
+        </div>
+        <div class="cardGrid">
+            <div class="interGrid">
+                <img src="/asset/picture/Smartphone.png" alt="">
+                <p>Handphone</p>
+            </div>
+            <div class="cardButton">
+                <a href="" style="text-decoration: none;">pilih</a>
+            </div>
+        </div>
+        <div class="cardGrid">
+            <div class="interGrid">
+                <img src="/asset/picture/Gaming.png" alt="">
+                <p>PC</p>
+            </div>
+            <div class="cardButton">
+                <a href="" style="text-decoration: none;">pilih</a>
+            </div>
+        </div>
+        <div class="cardGrid">
+            <div class="interGrid">
+                <img src="/asset/picture/Rj45.png" alt="">
+                <p>lan</p>
+            </div>
+            <div class="cardButton">
+                <a href="" style="text-decoration: none;">pilih</a>
+            </div>
+        </div>
+        <div class="cardGrid">
+            <div class="interGrid">
+                <img src="/asset/picture/Hdmi.png" alt="">
+                <p>HDMI</p>
+            </div>
+            <div class="cardButton">
+                <a href="" style="text-decoration: none;">pilih</a>
+            </div>
+        </div>
+        <div class="cardGrid">
+            <div class="interGrid">
+                <img src="/asset/picture/Projector.png" alt="">
+                <p>Projector</p>
+            </div>
+            <div class="cardButton">
+                <a href="" style="text-decoration: none;">pilih</a>
+            </div>
+        </div>
+        <div class="cardGrid">
+            <div class="interGrid">
+                <img src="/asset/picture/Hard disk.png" alt="">
+                <p>Hardisk</p>
+            </div>
+            <div class="cardButton">
+                <a href="" style="text-decoration: none;">pilih</a>
+            </div>
+        </div>
+        
+        
+
+    </div>
+
+    <!-- script -->
+    <script src="./dist/js/tabler.min.js?1684106062" defer></script>
+    <script src="./dist/js/demo.min.js?1684106062" defer></script>
+</body>
+
+</html>
