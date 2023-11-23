133e79e210cc6aea5c9b2fc758bad92fef4e0192
nblrbbni <muhammadrabbani463@smk.belajar.id>
2023-11-23 10:39:01 +0700

    information

--- resources/views/information/tampil.blade.php
+++ resources/views/information/tampil.blade.php
@@ -29,37 +29,37 @@ Information
     <!-- Page body -->
     <div class="page-body">
       <div class="container-xl">
-        <div class="card">
-          <div class="card-body">
-            <h1>Halaman Tambah</h1>
-            <a href="/information/create" class="btn btn-primary btn-sm mb-3">Tambah</a>
-            <div class="accordion" id="accordion-example">
-               @forelse ($information as $key => $value )
-               <div class="accordion-item">
-                    <h2 class="accordion-header">
-                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-1" aria-expanded="false">
-                            Pasal {{ $value->nama_pasal }}
-                        </button>
-                    </h2>
-                    <div id="collapse-1" class="accordion-collapse collapse " data-bs-parent="#accordion-example" >
-                        <div class="accordion-body pt-0">
-                            {{ $value->descripsi }}
+          <h1>Halaman Tambah</h1>
+          <a href="/information/create" class="btn btn-primary btn-sm mb-3">Tambah</a>
+            <div class="card">
+                <div class="card-body">
+                    <div class="accordion" id="accordion-example">
+                    @forelse ($information as $key => $value )
+                    <div class="accordion-item">
+                            <h2 class="accordion-header">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-1" aria-expanded="false">
+                                    Pasal {{ $value->nama_pasal }}
+                                </button>
+                            </h2>
+                            <div id="collapse-1" class="accordion-collapse collapse " data-bs-parent="#accordion-example" >
+                                <div class="accordion-body pt-0">
+                                    {{ $value->descripsi }}
+                                </div>
+                            </div>
                         </div>
+                    @empty
+                    <div class="accordion-item">
+                        <h2 class="accordion-header">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-1" aria-expanded="false">
+                                Tidak ada data
+                            </button>
+                        </h2>
+                    </div>
+                    @endforelse
                     </div>
                 </div>
-            @empty
-            <div class="accordion-item">
-                <h2 class="accordion-header">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-1" aria-expanded="false">
-                        Tidak ada data
-                    </button>
-                </h2>
             </div>
-            @endforelse
-            </div>
-          </div>
         </div>
-      </div>
     </div>
 </div>
 @endsection
