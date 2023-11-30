65356f2a95d2da27715dd4725d79249b8e3de509
nblrbbni <muhammadrabbani463@smk.belajar.id>
2023-11-30 15:13:27 +0700

    updateProfile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,8 +3,66 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class ProfileController extends Controller
 {
-    //
+    public function index(){
+            $users = Auth::id();
+            $detailprofile = User::where('id', $users)->first();
+
+            return view('profile.index', ['detailprofile' => $detailprofile]);
+        }
+    public function update(Request $request){
+
+        $request->validate([
+            'name'  => '',
+            'class'  => '',
+            'email' => '',
+            'password' => '',
+        ]);
+
+        $user = User::find(Auth::id());
+
+        $user->update([
+            'name' => $request->name,
+            'class' => $request->class,
+            'email' => $request->email,
+            'password' => $request->password ? bcrypt($request->password) : $user->password,
+        ]);
+
+        return redirect('/profile')->with('success', 'Profile updated successfully!');
+    }
+
+    // public function index(){
+    //     $users = Auth::id();
+    //     $detailprofile = User::where('id', $users)->first();
+
+    //     return view('profile.index', ['detailprofile' => $detailprofile]);
+    // }
+
+    // public function update(Request $request){
+
+    //     $request->validate([
+    //         'name'  => 'required',
+    //         'class'  => 'required',
+    //         'email' => 'required',
+    //         'password' => '',
+    //     ]);
+
+    //         dd($request->name);
+
+    //     $users= User::find();
+
+    //     $users->name = $request->name;
+    //     $users->class = $request->class;
+    //     $users->email = $request->email;
+    //     $users->password = $request->password;
+    //     $users->update();
+
+
+    //     return redirect('/profile');
+    // }
+
 }
